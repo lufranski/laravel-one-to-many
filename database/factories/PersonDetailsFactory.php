6ea37cb4b74f20a750d6f3adfcbb62c75c12342a
Federico Lufrano <105553021+lufranski@users.noreply.github.com>
2023-02-13 14:20:39 +0100

    created factories

---
+++ database/factories/PersonDetailsFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\PersonDetails>
+ */
+class PersonDetailsFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            
+            'phone' => fake() -> phoneNumber(),
+            'address' => fake() -> streetAddress(),
+            'avatar' => null
+        ];
+    }
+}
